dd8d97f6a8085fa45f9e475842d474b4091ef764
tony780515 <76420902+tony780515@users.noreply.github.com>
2021-08-09 15:55:05 +0800

    整理及微調程式碼

--- resources/views/applestore.blade.php
+++ resources/views/applestore.blade.php
@@ -4,7 +4,6 @@
 <link rel="stylesheet" href="{{mix('css/applestore.css')}}">
 @endsection
 
-
 @section('main')
 <div id="c1" class="content">
     <div class="banner"><img src="https://www.apple.com/v/home/z/images/heroes/imac/hero_imac__dqh65mwjj04m_largetall.jpg" alt=""></div>
@@ -51,43 +50,6 @@
             </div>
         </div>
         @endforeach
-        {{-- <div class="box">
-                <div class="photo"><a href=""><img
-                            src="https://s.yimg.com/zp/MerchandiseImages/bb2fdfc50a-Gd-9238370.jpg"
-                            alt=""></a></div>
-                <div class="text"><a href="">
-                        <h3>iPad Air</h3>
-                    </a></div>
-                <div class="link">
-                    <div class="link-text"><a href="">加入購物車</a> </div>
-                    <div class="link-icon"><i class="fas fa-angle-right"></i></div>
-                </div>
-
-            </div>
-            <div class="box">
-                <div class="photo"><a href=""><img
-                            src="https://store.storeimages.cdn-apple.com/8756/as-images.apple.com/is/ipad-2020-hero-landing?wid=744&hei=1212&fmt=jpeg&qlt=80&.v=1598913494000"
-                            alt=""></a></div>
-                <div class="text"><a href="">
-                        <h3>iPad</h3>
-                    </a></div>
-                <div class="link">
-                    <div class="link-text"><a href="">加入購物車</a> </div>
-                    <div class="link-icon"><i class="fas fa-angle-right"></i></div>
-                </div>
-            </div>
-            <div class="box">
-                <div class="photo"><a href=""><img
-                            src="https://store.storeimages.cdn-apple.com/4982/as-images.apple.com/is/ipad-mini-select-201911_FMT_WHH?wid=2000&hei=2000&fmt=jpeg&qlt=80&.v=1573825332486"
-                            alt=""></a></div>
-                <div class="text"><a href="">
-                        <h3>iPad mini</h3>
-                    </a></div>
-                <div class="link">
-                    <div class="link-text"><a href="">加入購物車</a> </div>
-                    <div class="link-icon"><i class="fas fa-angle-right"></i></div>
-                </div>
-            </div> --}}
     </div>
 </div>
 <div id="c3" class="content">
@@ -113,43 +75,6 @@
             </div>
         </div>
         @endforeach
-        {{-- <div class="box">
-                <div class="photo"><a href=""><img
-                            src="https://s.yimg.com/zp/MerchandiseImages/3279D4BDC1-SP-9795825.jpg"
-                            alt=""></a></div>
-                <div class="text"><a href="">
-                        <h3>iPhone 12</h3>
-                    </a></div>
-                <div class="link">
-                    <div class="link-text"><a href="">加入購物車</a> </div>
-                    <div class="link-icon"><i class="fas fa-angle-right"></i></div>
-                </div>
-
-            </div>
-            <div class="box">
-                <div class="photo"><a href=""><img
-                            src="https://store.storeimages.cdn-apple.com/8756/as-images.apple.com/is/iphone-se-family-select-2020?wid=882&hei=1058&fmt=jpeg&qlt=80&.v=1586794486444"
-                            alt=""></a></div>
-                <div class="text"><a href="">
-                        <h3>iPhone SE</h3>
-                    </a></div>
-                <div class="link">
-                    <div class="link-text"><a href="">加入購物車</a> </div>
-                    <div class="link-icon"><i class="fas fa-angle-right"></i></div>
-                </div>
-            </div>
-            <div class="box">
-                <div class="photo"><a href=""><img
-                            src="https://store.storeimages.cdn-apple.com/8756/as-images.apple.com/is/iphone11-select-2019-family?wid=882&hei=1058&fmt=jpeg&qlt=80&.v=1567022175704"
-                            alt=""></a></div>
-                <div class="text"><a href="">
-                        <h3>iPhone 11</h3>
-                    </a></div>
-                <div class="link">
-                    <div class="link-text"><a href="">加入購物車</a> </div>
-                    <div class="link-icon"><i class="fas fa-angle-right"></i></div>
-                </div>
-            </div> --}}
     </div>
 </div>
 <div id="c4" class="content">
@@ -176,44 +101,6 @@
             </div>
         </div>
         @endforeach
-        {{-- <div class="box">
-                <div class="photo"><a href=""><img
-                            src="https://www.apple.com/v/watch/shared/compare/e/images/overview/compare_se__crebhd9hhdea_large.jpg"
-                            alt=""></a></div>
-                <div class="text"><a href="">
-                        <h3>Apple Watch SE</h3>
-                    </a></div>
-                <div class="link">
-                    <div class="link-text"><a href="">加入購物車</a> </div>
-                    <div class="link-icon"><i class="fas fa-angle-right"></i></div>
-                </div>
-
-            </div>
-            <div class="box">
-                <div class="photo"><a href=""><img
-                            src="https://store.storeimages.cdn-apple.com/8756/as-images.apple.com/is/42-alu-space-sport-black-nc-s3-grid_GEO_TW?wid=270&hei=275&fmt=jpeg&qlt=95&.v=1594259810000"
-                            alt=""></a></div>
-                <div class="text"><a href="">
-                        <h3>Apple Watch Series 3</h3>
-                    </a></div>
-                <div class="link">
-                    <div class="link-text"><a href="">加入購物車</a> </div>
-                    <div class="link-icon"><i class="fas fa-angle-right"></i></div>
-                </div>
-            </div>
-            <div class="box">
-                <div class="photo"><a href=""><img
-                            src="https://store.storeimages.cdn-apple.com/8756/as-images.apple.com/is/MG213_VW_34FR+watch-44-stainless-silver-cell-hermes6s_VW_34FR_WF_CO+watch-face-44-hermes6s-encre_VW_34FR_WF_CO_GEO_TW?wid=375&hei=356&trim=1,0&fmt=p-jpg&qlt=95&.v=1599351644000,1600811023000,1618108900000"
-                            alt=""></a></div>
-                <div class="text"><a href="">
-                        <h3>Apple Watch Hermès
-                        </h3>
-                    </a></div>
-                <div class="link">
-                    <div class="link-text"><a href="">加入購物車</a> </div>
-                    <div class="link-icon"><i class="fas fa-angle-right"></i></div>
-                </div>
-            </div> --}}
     </div>
 </div>
 <div class="backtotop"><a href="#"><img
