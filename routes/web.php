86059efa41d8433cea5705bec7a42ba6c03d262a
tony780515 <76420902+tony780515@users.noreply.github.com>
2021-08-12 15:43:49 +0800

    程式碼整理

--- routes/web.php
+++ routes/web.php
@@ -17,13 +17,13 @@ use Illuminate\Support\Facades\Route;
 //     return view('shoppingcart');
 // });
 
-Route::get('/','App\Http\Controllers\ProductController@index');
-Route::get('/shoppingcart','App\Http\Controllers\CartController@index');
-Route::get('/shoppingcart/store/{id}','App\Http\Controllers\CartController@store');
-Route::get('/shoppingcart/add/{id}','App\Http\Controllers\CartController@add');
-Route::get('/shoppingcart/minus/{id}','App\Http\Controllers\CartController@minus');
-Route::get('/shoppingcart/destroy/{id}','App\Http\Controllers\CartController@destroy');
+Route::get('/', 'App\Http\Controllers\ProductController@index');
+Route::get('/shoppingcart', 'App\Http\Controllers\CartController@index');
+Route::get('/shoppingcart/store/{id}', 'App\Http\Controllers\CartController@store');
+Route::get('/shoppingcart/add/{id}', 'App\Http\Controllers\CartController@add');
+Route::get('/shoppingcart/minus/{id}', 'App\Http\Controllers\CartController@minus');
+Route::get('/shoppingcart/destroy/{id}', 'App\Http\Controllers\CartController@destroy');
 
-Route::get('/orderlist/{id}','App\Http\Controllers\OrderController@show')->name('orderlist');
-Route::post('/orderlist/store','App\Http\Controllers\OrderController@store');
+Route::get('/orderlist/{id}', 'App\Http\Controllers\OrderController@show')->name('orderlist');
+Route::post('/orderlist/store', 'App\Http\Controllers\OrderController@store');
 
